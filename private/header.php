ec7f241ed2c1730343e227637b6459023a78ab8c
Viacheslav <dvo@tuta.io>
2022-07-04 20:06:01 +0300

    Linked css and js to html

--- private/header.php
+++ private/header.php
@@ -4,5 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title ?></title>
+    <link rel="stylesheet" href="/private/style.css">
+    <script src="private/js/jquery-3.6.0.min.js"></script>
+    <script src="private/js/main.js"></script>
 </head>
 <body>
