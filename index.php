353cdff1a00f17f3d1239750a2d07ea63c6918a6
Viacheslav <dvo@tuta.io>
2022-07-04 20:11:33 +0300

    Created index page

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$title = 'Главная';
+require_once('private/header.php');
+
+?>
+
+<h2><a href="/login.php">Авторизация</a></h2>
+<h2><a href="/signup.php">Регистрация</a></h2>
+
+
+<?php
+
+require_once('private/footer.php');
+
+?>
