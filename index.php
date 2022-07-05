3fd1624af728dc9bb3aea4aa0595584ab4370ade
Viacheslav <dvo@tuta.io>
2022-07-05 11:57:20 +0300

    Added user status on main page

--- index.php
+++ index.php
@@ -5,6 +5,15 @@ require_once('private/php/header.php');
 
 ?>
 
+<h1>Статус пользователя: 
+    <?php 
+        if(isset($_SESSION['user_id'])){
+            echo "авторизован";
+        } else {
+            echo "не авторизован";
+        }
+    ?>
+</h1>
 <h2><a href="/login.php">Авторизация</a></h2>
 <h2><a href="/signup.php">Регистрация</a></h2>
 
